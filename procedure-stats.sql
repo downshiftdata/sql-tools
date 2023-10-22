f93fff9581e04414e994911d5ae39bf930ae5d79
J <downshiftdata@gmail.com>
2023-10-22 10:25:36 -0400

    better stats queries, remove tabs

--- procedure-stats.sql
+++ procedure-stats.sql
@@ -1,28 +1,57 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SET NOCOUNT ON;
-WITH cteStats AS (SELECT TOP 100 ps.[plan_handle]
-	FROM sys.dm_exec_procedure_stats AS ps
-	WHERE DATEDIFF(hh, ps.[cached_time], GETDATE()) >= 1
-		AND EXISTS (SELECT 1
-				FROM sys.dm_exec_cached_plans AS cp
-				WHERE ps.[plan_handle] = cp.[plan_handle])
-	ORDER BY ps.[total_elapsed_time] / DATEDIFF(hh, ps.[cached_time], GETDATE()) DESC)
+DROP TABLE IF EXISTS #ps;
+CREATE TABLE #ps (
+    [database_id] INT NOT NULL,
+    [object_id] INT NOT NULL,
+    [plan_handle] VARBINARY(64) NOT NULL,
+    [cached_time] DATETIME NOT NULL,
+    [execution_count] BIGINT NOT NULL,
+    [total_worker_time] BIGINT NOT NULL,
+    [total_physical_reads] BIGINT NOT NULL,
+    [total_logical_writes] BIGINT NOT NULL,
+    [total_logical_reads] BIGINT NOT NULL,
+    [total_elapsed_time] BIGINT NOT NULL,
+    PRIMARY KEY ([plan_handle]));
+INSERT INTO #ps (
+        [database_id],
+        [object_id],
+        [plan_handle],
+        [cached_time],
+        [execution_count],
+        [total_worker_time],
+        [total_physical_reads],
+        [total_logical_writes],
+        [total_logical_reads],
+        [total_elapsed_time])
+    SELECT TOP (1000)
+            ps.[database_id],
+            ps.[object_id],
+            ps.[plan_handle],
+            ps.[cached_time],
+            ps.[execution_count],
+            ps.[total_worker_time],
+            ps.[total_physical_reads],
+            ps.[total_logical_writes],
+            ps.[total_logical_reads],
+            ps.[total_elapsed_time]
+        FROM sys.dm_exec_procedure_stats AS ps
+        WHERE DATEDIFF(mi, ps.[cached_time], GETDATE()) >= 1
+        ORDER BY ps.[total_elapsed_time] / DATEDIFF(mi, ps.[cached_time], GETDATE()) DESC;
 SELECT
-		DB_NAME(ps.[database_id]) + N'.' + OBJECT_SCHEMA_NAME(ps.[object_id]) + N'.' + OBJECT_NAME(ps.[object_id]) AS [full_name],
-		ps.[cached_time],
-		cp.[size_in_bytes] / 1024 AS [cache_size_kb],
-		ps.[execution_count] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [exec_per_hour],
-		ps.[total_worker_time] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [worker_time_per_hour],
-		ps.[total_physical_reads] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [physical_reads_per_hour],
-		ps.[total_logical_writes] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [logical_writes_per_hour],
-		ps.[total_logical_reads] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [logical_reads_per_hour],
-		ps.[total_elapsed_time] / DATEDIFF(hh, ps.[cached_time], GETDATE()) AS [elapsed_time_per_hour],
-		qp.[query_plan]
-	FROM cteStats AS s
-		INNER JOIN sys.dm_exec_cached_plans AS cp
-			ON s.[plan_handle] = cp.[plan_handle]
-		INNER JOIN sys.dm_exec_procedure_stats AS ps
-			ON cp.[plan_handle] = ps.[plan_handle]
-		CROSS APPLY sys.dm_exec_query_plan(s.[plan_handle]) AS qp
-	ORDER BY ps.[total_elapsed_time] / DATEDIFF(hh, ps.[cached_time], GETDATE()) DESC
-	OPTION (RECOMPILE);
+        DB_NAME(ps.[database_id]) + N'.' + OBJECT_SCHEMA_NAME(ps.[object_id]) + N'.' + OBJECT_NAME(ps.[object_id]) AS [full_name],
+        ps.[cached_time],
+        cp.[size_in_bytes] / 1024 AS [cache_size_kb],
+        ps.[execution_count] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [exec_per_min],
+        ps.[total_worker_time] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [worker_time_per_min],
+        ps.[total_physical_reads] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [physical_reads_per_min],
+        ps.[total_logical_writes] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [logical_writes_per_min],
+        ps.[total_logical_reads] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [logical_reads_per_min],
+        ps.[total_elapsed_time] / DATEDIFF(mi, ps.[cached_time], GETDATE()) AS [elapsed_time_per_min],
+        qp.[query_plan]
+    FROM sys.dm_exec_procedure_stats AS ps
+        INNER JOIN sys.dm_exec_cached_plans AS cp
+            ON ps.[plan_handle] = cp.[plan_handle]
+        CROSS APPLY sys.dm_exec_query_plan(ps.[plan_handle]) AS qp
+    ORDER BY ps.[total_elapsed_time] / DATEDIFF(mi, ps.[cached_time], GETDATE()) DESC
+    OPTION (RECOMPILE);
--- query-stats.sql
+++ query-stats.sql
@@ -1,36 +1,69 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SET NOCOUNT ON;
-WITH cteStats AS (SELECT TOP 100 qs.[plan_handle]
-	FROM sys.dm_exec_query_stats AS qs
-	WHERE DATEDIFF(hh, qs.[creation_time], GETDATE()) >= 1
-		AND EXISTS (SELECT 1
-				FROM sys.dm_exec_cached_plans AS cp
-				WHERE qs.[plan_handle] = cp.[plan_handle])
-	ORDER BY qs.[total_elapsed_time] / DATEDIFF(hh, qs.[creation_time], GETDATE()) DESC)
+DROP TABLE IF EXISTS #qs;
+CREATE TABLE #qs (
+    [plan_handle] VARBINARY(64) NOT NULL,
+    [plan_generation_num] BIGINT NOT NULL,
+    [i] INT IDENTITY(1,1) NOT NULL,
+    [statement_start_offset] INT NOT NULL,
+    [statement_end_offset] INT NOT NULL,
+    [creation_time] DATETIME NOT NULL,
+    [execution_count] BIGINT NOT NULL,
+    [total_worker_time] BIGINT NOT NULL,
+    [total_physical_reads] BIGINT NOT NULL,
+    [total_logical_writes] BIGINT NOT NULL,
+    [total_logical_reads] BIGINT NOT NULL,
+    [total_elapsed_time] BIGINT NOT NULL,
+    PRIMARY KEY ([plan_handle], [plan_generation_num], [i]));
+INSERT INTO #qs (
+        [plan_handle],
+        [plan_generation_num],
+        [statement_start_offset],
+        [statement_end_offset],
+        [creation_time],
+        [execution_count],
+        [total_worker_time],
+        [total_physical_reads],
+        [total_logical_writes],
+        [total_logical_reads],
+        [total_elapsed_time])
+    SELECT TOP (1000)
+            qs.[plan_handle],
+            qs.[plan_generation_num],
+            qs.[statement_start_offset],
+            qs.[statement_end_offset],
+            qs.[creation_time],
+            qs.[execution_count],
+            qs.[total_worker_time],
+            qs.[total_physical_reads],
+            qs.[total_logical_writes],
+            qs.[total_logical_reads],
+            qs.[total_elapsed_time]
+        FROM sys.dm_exec_query_stats AS qs
+        WHERE DATEDIFF(mi, qs.[creation_time], GETDATE()) >= 1
+        ORDER BY qs.[total_elapsed_time] / DATEDIFF(mi, qs.[creation_time], GETDATE()) DESC;
 SELECT
-		SUBSTRING(st.[text],
-			qs.[statement_start_offset] / 2 + 1,
-			CASE qs.[statement_end_offset]
-					WHEN -1 THEN DATALENGTH(st.[text])
-					ELSE qs.[statement_end_offset]
-				END - qs.[statement_start_offset] / 2 + 1) AS [query],
-		cp.[objtype],
-		qp.[query_plan].value(N'(//@StatementText)[1]', N'NVARCHAR(MAX)') AS [stmt1],
-		qs.[creation_time],
-		cp.[size_in_bytes] / 1024 AS [cache_size_kb],
-		qs.[execution_count] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [exec_per_hour],
-		qs.[total_worker_time] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [worker_time_per_hour],
-		qs.[total_physical_reads] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [physical_reads_per_hour],
-		qs.[total_logical_writes] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [logical_writes_per_hour],
-		qs.[total_logical_reads] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [logical_reads_per_hour],
-		qs.[total_elapsed_time] / DATEDIFF(hh, qs.[creation_time], GETDATE()) AS [elapsed_time_per_hour],
-		qp.[query_plan]
-	FROM cteStats AS s
-		INNER JOIN sys.dm_exec_cached_plans AS cp
-			ON s.[plan_handle] = cp.[plan_handle]
-		INNER JOIN sys.dm_exec_query_stats AS qs
-			ON cp.[plan_handle] = qs.[plan_handle]
-		CROSS APPLY sys.dm_exec_query_plan(s.[plan_handle]) AS qp
-		CROSS APPLY sys.dm_exec_sql_text(s.[plan_handle]) AS st
-	ORDER BY qs.[total_elapsed_time] / DATEDIFF(hh, qs.[creation_time], GETDATE()) DESC
-	OPTION (RECOMPILE);
+        SUBSTRING(st.[text],
+            qs.[statement_start_offset] / 2 + 1,
+            CASE qs.[statement_end_offset]
+                    WHEN -1 THEN DATALENGTH(st.[text])
+                    ELSE qs.[statement_end_offset]
+                END - qs.[statement_start_offset] / 2 + 1) AS [query],
+        cp.[objtype],
+        qp.[query_plan].value(N'(//@StatementText)[1]', N'NVARCHAR(MAX)') AS [stmt1],
+        qs.[creation_time],
+        cp.[size_in_bytes] / 1024 AS [cache_size_kb],
+        qs.[execution_count] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [exec_per_min],
+        qs.[total_worker_time] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [worker_time_per_min],
+        qs.[total_physical_reads] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [physical_reads_per_min],
+        qs.[total_logical_writes] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [logical_writes_per_min],
+        qs.[total_logical_reads] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [logical_reads_per_min],
+        qs.[total_elapsed_time] / DATEDIFF(mi, qs.[creation_time], GETDATE()) AS [elapsed_time_per_min],
+        qp.[query_plan]
+    FROM #qs AS qs
+        INNER JOIN sys.dm_exec_cached_plans AS cp
+            ON qs.[plan_handle] = cp.[plan_handle]
+        CROSS APPLY sys.dm_exec_query_plan(qs.[plan_handle]) AS qp
+        CROSS APPLY sys.dm_exec_sql_text(qs.[plan_handle]) AS st
+    ORDER BY qs.[total_elapsed_time] / DATEDIFF(mi, qs.[creation_time], GETDATE()) DESC
+    OPTION (RECOMPILE);
